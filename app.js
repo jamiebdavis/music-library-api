7f9f079b8a767377f145ad9e205c9eafc029d382
Jamie Davis <jamied1985@gmail.com>
2018-09-19 15:55:52 +0100

    need to test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const bodyParser = require("body-parser");
+const { post, list, get } = require("./controllers/artist");
 
 require("dotenv").config({
   path: path.join(__dirname, "./settings.env")
@@ -18,6 +19,12 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Hello Mongo DB");
 });
+
+app.post("/artist", post);
+
+app.get("/artist", list);
+
+app.get("/artist/:artistId", get);
 
 app.listen(3000, () => {
   console.log("It Works!!, Listening on port 3000");
---
+++ controllers/artist.js
@@ -0,0 +1,26 @@
+const Artist = require("../models/artist");
+
+exports.post = (req, res) => {
+  const artist = new Artist({ name: req.body.name, genre: req.body.genre });
+  artist.save((err, artistCreated) => {
+    res.json(artistCreated);
+  });
+};
+
+exports.list = (req, res) => {
+  Artist.find({}, (err, artists) => {
+    if (err) {
+      res.json("something went wrong in list");
+    }
+    res.json(artists);
+  });
+};
+
+exports.get = (req, res) => {
+  Artist.findById(req.params.artistId, (err, artist) => {
+    if (err) {
+      res.json("Something went wrong");
+    }
+    res.json(artist);
+  });
+};
